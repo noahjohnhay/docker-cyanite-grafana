6f4980025151910f3ea0334f5952e45e8e220a5a
Noah Hay <noahjohnhay@gmail.com>
2016-10-17 12:58:47 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 echo "====================================="
 echo "=         BUILDING CYANITE          ="
 echo "====================================="
-cd ../cyanite
+cd cyanite
 docker build --tag="cyanite:0.5.1" .
 sleep 3
 docker run -d -p 2003:2003 --name="cyanite" cyanite:0.5.1
