71bc3dcd8d8b3bc995883e0d23313f9e2ae0672c
Noah Hay <noahjohnhay@gmail.com>
2016-10-17 13:53:37 -0300

    Update cyanite.sh

--- cyanite/files/opt/cyanite/cyanite.sh
+++ cyanite/files/opt/cyanite/cyanite.sh
@@ -6,11 +6,9 @@ fi
 
 cat /opt/cyanite/schema.cql | cqlsh us-east-1-dev-dse.cxengagelabs.net
 
-CYANITE_VERSION="0.5.1"
-
-JAR="$EXTRA_CLASSPATH:/opt/cyanite/cyanite-${CYANITE_VERSION}-standalone.jar"
+JAR="/opt/cyanite/cyanite-0.5.1-standalone.jar"
 CONFIG="/etc/cyanite.yaml"
 
 EXTRA_JAVA_OPTS="-Xmx1024m -Xms1024m"
 
-exec java $EXTRA_JAVA_OPTS $OPTS -cp "$JAR" io.cyanite "$CONFIG"
+exec java $EXTRA_JAVA_OPTS $OPTS -jar "$JAR" -f "$CONFIG"
